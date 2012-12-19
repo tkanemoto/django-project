692c8ecb6bb6d8f46b5a0af547ca03ff71f173f7
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2012-12-19 22:13:59 +0900

    Added and configured basic-apps and its dependencies
    Change-Id: I2f48bd0301c38ab0fa67f0c0d97a53b4c7efda7a

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -116,9 +116,32 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    # Basic Apps dependencies
+    'django.contrib.comments',
+    'django.contrib.markup',
+    'tagging',
+    # Basic Apps
+    'basic.blog',
+    'basic.bookmarks',
+    'basic.books',
+    'basic.comments',
+    'basic.events',
+    'basic.flagging',
+    'basic.groups',
+    'basic.inlines',
+    'basic.invitations',
+    'basic.media',
+    'basic.messages',
+    'basic.movies',
+    'basic.music',
+    'basic.people',
+    'basic.places',
+    'basic.profiles',
+    'basic.relationships',
+    'basic.tools',
 )
 
 # A sample logging configuration. The only tangible logging
--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,8 +10,31 @@ urlpatterns = patterns('',
     # url(r'^stage/', include('stage.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+
+    # Basic Apps dependencies
+    (r'^comments/', include('django.contrib.comments.urls')),
+    # Basic Apps
+    url(r'^blog/', include('basic.blog.urls')),
+    url(r'^bookmarks/', include('basic.bookmarks.urls')),
+    url(r'^books/', include('basic.books.urls')),
+    url(r'^blog-comments/', include('basic.events.urls')),
+    url(r'^flagging/', include('basic.flagging.urls')),
+    url(r'^groups/', include('basic.groups.urls')),
+    #url(r'^inlines/', include('basic.inlines.urls')),
+    url(r'^invitations/', include('basic.invitations.urls')),
+    # url(r'^media/', include('basic.media.urls')),
+    url(r'^media/photos', include('basic.media.urls.photos')),
+    url(r'^media/videos', include('basic.media.urls.videos')),
+    url(r'^messages/', include('basic.messages.urls')),
+    url(r'^movies/', include('basic.movies.urls')),
+    url(r'^music/', include('basic.music.urls')),
+    url(r'^people/', include('basic.people.urls')),
+    url(r'^places/', include('basic.places.urls')),
+    url(r'^profiles/', include('basic.profiles.urls')),
+    url(r'^relationships/', include('basic.relationships.urls')),
+    # url(r'^tools/', include('basic.tools.urls')),
 )
