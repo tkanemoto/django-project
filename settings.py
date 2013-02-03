c1ffee1cc11b03238c0e918bf9cd6e870de42a3b
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2013-02-03 17:36:02 +0900

    Added default template context processors
    Change-Id: I790563c0d9576d7c30c6711210a420c94e3d3934

--- settings.py
+++ settings.py
@@ -114,6 +114,16 @@ TEMPLATE_DIRS = (
     join(PROJECT_ROOT, 'templates')
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
+]
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
