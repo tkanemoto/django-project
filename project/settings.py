b3f3dcf92253f82a592f5ad3b57365ef1e157e82
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2016-06-20 00:01:36 +0100

    Enable django.contrib.sites.middleware.CurrentSiteMiddleware

--- project/settings.py
+++ project/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
 )
 
 ROOT_URLCONF = '{0}.urls'.format(PROJECT_NAME)
